ede045ab84627f381ec01f76c8ad7ed29f7276f9
MOIN26 <182437333+MOINtwentysix@users.noreply.github.com>
2026-09-02 13:03:30 +0000

    Hopefully a Fix :) #11

--- metro.config.js
+++ metro.config.js
@@ -3,21 +3,31 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.watchFolders = [__dirname];
+config.watchFolders = [
+  path.resolve(__dirname, 'src'),
+  path.resolve(__dirname, 'assets'),
+];
 
 const nodeModules = path.resolve(__dirname, 'node_modules');
 const srcDir = path.resolve(__dirname, 'src');
 const rootDir = __dirname;
+const assetsDir = path.resolve(__dirname, 'assets');
 
 config.resolver.extraNodeModules = (request) => {
   if (request === '@') {
     return srcDir;
   }
-  if (request.startsWith('@assets/')) {
-    return path.join(rootDir, request.slice(1));
+  if (request.startsWith('@/assets/')) {
+    return path.join(assetsDir, request.slice(9));
   }
   if (request.startsWith('@/')) {
     return path.join(srcDir, request.slice(2));
+  }
+  if (request.startsWith('@assets/')) {
+    return path.join(assetsDir, request.slice(8));
+  }
+  if (request.startsWith('assets/')) {
+    return path.join(assetsDir, request.slice(7));
   }
   return path.join(nodeModules, request);
 };
